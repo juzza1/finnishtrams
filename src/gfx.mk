84e983a620d0045eb0d39f328734cdf61a26b678
juzza1 <juzza1@gmail.com>
2017-11-01 23:51:54 +0200

    arctic (test only)

--- src/gfx.mk
+++ src/gfx.mk
@@ -9,3 +9,9 @@ build/test_6a.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6_arti
 build/test_8.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 8
+build/arctic_front.png: src/gfx/xcf/vehs/arctic.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg front cat
+build/arctic_middle.png: src/gfx/xcf/vehs/arctic.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg middle
+build/arctic_rear.png: src/gfx/xcf/vehs/arctic.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg rear
