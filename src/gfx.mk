7a5dc1bfdc7ed7b13d615e762e8effb8c685c78d
juzza1 <juzza1@gmail.com>
2017-11-01 19:16:11 +0200

    more test vehicles, more compact template

--- src/gfx.mk
+++ src/gfx.mk
@@ -1,7 +1,11 @@
 XCFP := xcf2png
 XCFP_FLAGS := --autocrop --background \\\#FFFFFF --dissolve --output
 
-build/test_8.png:
-	$(XCFP) $(XCFP_FLAGS) $@ src/gfx/xcf/testveh.xcf bg 8
-build/test_4.png:
-	$(XCFP) $(XCFP_FLAGS) $@ src/gfx/xcf/testveh.xcf bg 4
+build/test_4.png: src/gfx/xcf/testveh.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg 4
+build/test_6.png: src/gfx/xcf/testveh.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6
+build/test_6a.png: src/gfx/xcf/testveh.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6_arti
+build/test_8.png: src/gfx/xcf/testveh.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg 8
