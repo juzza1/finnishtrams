063d3a01f5510f29d37cb15fc4f4782d622fd6bd
juzza1 <juzza1@gmail.com>
2017-11-01 19:46:16 +0200

    Move actual vehicles to own dir

--- src/gfx.mk
+++ src/gfx.mk
@@ -1,11 +1,11 @@
 XCFP := xcf2png
 XCFP_FLAGS := --autocrop --background \\\#FFFFFF --dissolve --output
 
-build/test_4.png: src/gfx/xcf/testveh.xcf
+build/test_4.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 4
-build/test_6.png: src/gfx/xcf/testveh.xcf
+build/test_6.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6
-build/test_6a.png: src/gfx/xcf/testveh.xcf
+build/test_6a.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6_arti
-build/test_8.png: src/gfx/xcf/testveh.xcf
+build/test_8.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 8
