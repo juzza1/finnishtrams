83ea518ce23f9fa1f0f4aa93b73ec40c70a29f28
juzza1 <juzza1@gmail.com>
2017-11-02 18:43:46 +0200

    thin test vehicles

--- src/gfx.mk
+++ src/gfx.mk
@@ -3,12 +3,16 @@ XCFP_FLAGS := --autocrop --background \\\#FFFFFF --dissolve --output
 
 build/test_4.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 4
+build/test4_6w.png: src/gfx/xcf/vehs/testveh.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg 4_6w
 build/test_6.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6
 build/test_6a.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 6_arti
 build/test_8.png: src/gfx/xcf/vehs/testveh.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 8
+build/test8_6w.png: src/gfx/xcf/vehs/testveh.xcf
+	$(XCFP) $(XCFP_FLAGS) $@ $< bg 8_6w
 build/mlnrviii_front.png: src/gfx/xcf/vehs/mlnrviii.xcf
 	$(XCFP) $(XCFP_FLAGS) $@ $< bg 1 panto
 build/mlnrviii_middle.png: src/gfx/xcf/vehs/mlnrviii.xcf
